a829a9fa7b9ae240fa36654c2264f348106de977
laperson <43199823+laperson@users.noreply.github.com>
2023-04-30 22:15:15 +0300

    feat: sketch a layout

--- scripts/element-creater.js
+++ scripts/element-creater.js
@@ -1,19 +1,21 @@
 import eCode from './e_code.js';
 
 document.body.innerHTML = `<div class="wrapper">
-                             <header class="header"><h1 class="text_header">RSS Virtual Keyboard</h1></header>
+                             <header class="header">
+                               <h1 class="text_header">RSS Virtual Keyboard</h1>
+                               <div class="contain_descript">
+                                 <p class="text_descript">The keyboard was created in the Windows operating system.</p>
+                                 <p class="text_descript">To switch the language, the combination is: left ctrl + alt.</p>
+                               </div>
+                             </header>
                              <main class="main">
-                               <textarea class="field"></textarea>
+                               <textarea class="field" autofocus></textarea>
                                <div class="keyboard"></div>
-                               <footer class="footer">
-                                 <p class="text_footer">The keyboard was created in the Windows operating system.</p>
-                                 <p class="text_footer">To switch the language, the combination is: left ctrl + alt.</p>
-                               </footer>
                              </main>
                            </div>`;
 
 eCode.forEach((el) => {
   const key = document.createElement('div');
-  key.className = `key ${el}`;
+  key.className = `key ${el.toLocaleLowerCase()}`;
   document.querySelector('.keyboard').append(key);
 });
